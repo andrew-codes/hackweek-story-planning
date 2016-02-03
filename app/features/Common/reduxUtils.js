995bb87cfd7cfd752ee1eccc60ca9881ce725d68
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2016-02-03 10:47:04 -0500

    Update styles, add load user to save other user info (avatar, etc.)

--- app/features/Common/reduxUtils.js
+++ app/features/Common/reduxUtils.js
@@ -1,7 +1,5 @@
-import {
-	createStore as reduxCreateStore, applyMiddleware, combineReducers
-}
-	from 'redux';
+import {  createStore as reduxCreateStore, applyMiddleware, combineReducers, compose}  from 'redux';
+import devTools from 'remote-redux-devtools';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise';
 import {isApp} from './env';
@@ -9,13 +7,13 @@ import {isApp} from './env';
 export const createProxyActionCreator = createAction => (actionType, payload) => createAction(`server/${actionType}`, payload);
 
 export const createStore = url => {
-	const middlewares = [thunk, promise];
-	if (isApp) {
-		window.navigator.userAgent = 'react-native';
-		const createSocketIoMiddleware = require('redux-socket.io');
-		const io = require('socket.io-client/socket.io');
-		const socketIoMiddleware = createSocketIoMiddleware(io(url), 'server/');
-		middlewares.push(socketIoMiddleware);
-	}
-	return applyMiddleware(...middlewares)(reduxCreateStore);
+  const middlewares = [thunk, promise];
+  if (isApp) {
+    window.navigator.userAgent = 'react-native';
+    const createSocketIoMiddleware = require('redux-socket.io');
+    const io = require('socket.io-client/socket.io');
+    const socketIoMiddleware = createSocketIoMiddleware(io(url), 'server/');
+    middlewares.push(socketIoMiddleware);
+  }
+  return compose(applyMiddleware(...middlewares), devTools())(reduxCreateStore);
 };
